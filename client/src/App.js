7ff50a1674ca183efc215ad08f9bfa3386b9652a
oliviasylee <oliviasylee@gmail.com>
2023-04-08 00:54:01 -0400

    Add Footer component and update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Nav from './components/Nav/index';
-// import Footer from './components/Footer/index'
+import Footer from './components/Footer/index';
 import Container from '@mui/material/Container';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="app-wrapper" style={{ borderBottom: '2px solid #000', marginTop: '40px', padding: '16px'  }}>
-      <Container maxWidth='lg'>
-        <Nav />
-      </Container>
-    </div>
+      <div className='app-wrapper' style={{ borderBottom: '3px solid #000', marginTop: '40px', padding: '16px' }}>
+        <Container maxWidth='lg'>
+          <Nav />
+        </Container>
+      </div>
+      <div className='app-content'>
+        {/* home */}
+      </div>
+      <Footer />
     </BrowserRouter>
   );
 }
-
 
 export default App;
