e2dd855174ba832272c8b69c5104b335ce02ab26
oliviasylee <oliviasylee@gmail.com>
2023-04-10 00:15:39 -0400

    Add loading state and error handling to CategoryMenu component

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,4 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import { useStoreContext } from '../../utils/GlobalState';
+import {
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+} from '../../utils/actions';
+import { QUERY_CATEGORIES } from '../../utils/queries';
+import { idbPromise } from '../../utils/helpers';
 
 import { 
   Container,
@@ -8,21 +16,66 @@ import {
 import FilterListIcon from '@mui/icons-material/FilterList';
 
 function CategoryMenu() {
+  
+  const [state, dispatch] = useStoreContext();
+
+  const { categories } = state;
+
+  const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+  useEffect(() => {
+    if (categoryData) {
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories,
+      });
+      categoryData.categories.forEach((category) => {
+        idbPromise('categories', 'put', category);
+      });
+    } else if (!loading) {
+      idbPromise('categories', 'get').then((categories) => {
+        dispatch({
+          type: UPDATE_CATEGORIES,
+          categories: categories,
+        });
+      });
+    }
+  }, [categoryData, loading, dispatch]);
+
+  const handleClick = (id) => {
+    dispatch({
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: id,
+    });
+  };
+
   return (
     <Container maxWidth='lg'>
       <Grid container>
         <Grid item xs={12}>
-          <h2>Filter by <FilterListIcon fontSize='small' style={{ color: 'black' }}></FilterListIcon></h2>
-          <Button 
-            variant='outlined'
-            size='medium'
-            color='info'>
-            Coffee
-          </Button>
+          <h2>Filter by <FilterListIcon fontSize='small' style={{ color: 'black' }} /></h2>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            categories.map((item) => (
+              <Button
+                key={item._id}
+                variant='outlined'
+                size='medium'
+                color='info'
+                onClick={() => {
+                  handleClick(item._id);
+                }}
+              >
+                {item.name}
+              </Button>
+            ))
+          )}
         </Grid>
       </Grid>
     </Container>
   );
+  
 }
 
 export default CategoryMenu;
