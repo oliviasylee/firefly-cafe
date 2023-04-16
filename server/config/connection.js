ed8ba23bff2a9c9903efbd55f3854272d75e93db
minjae <jaecho203@gmail.com>
2023-04-16 18:47:51 -0400

    heroku cluster

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/fluffygoogles', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cluster0.fuds4wh.mongodb.net', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
