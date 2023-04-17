1372058c2f1075463af3208176605611e47c568b
minjae <jaecho203@gmail.com>
2023-04-16 22:50:51 -0400

    line_items fix

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -88,7 +88,7 @@ const resolvers = {
       
       const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
-        line_items: line_items,
+        line_items,
         mode: 'payment',
         success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${url}/`
